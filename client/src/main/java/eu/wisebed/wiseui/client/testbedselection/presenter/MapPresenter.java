0e42372bc95eb0215868a62544d961b40f65039e
mlegenhausen <mlegenhausen@gmail.com>
2011-03-14 17:34:25 +0100

    Removed sysout and debugging code.

--- client/src/main/java/eu/wisebed/wiseui/client/testbedselection/presenter/MapPresenter.java
+++ client/src/main/java/eu/wisebed/wiseui/client/testbedselection/presenter/MapPresenter.java
@@ -52,9 +52,7 @@ public class MapPresenter implements MapView.Presenter, WisemlLoadedHandler, Con
         final Setup setup = event.getWiseml().getSetup();
         final Coordinate origin = setup.getOrigin();
         view.setTestbedCoordinate(origin, configuration.getName(), setup.getDescription());
-        origin.setPhi(-145.0);
         final Coordinate cartesian = Coordinates.blh2xyz(origin);
-        System.out.println(cartesian);
         final List<Coordinate> coordinates = Lists.transform(setup.getNode(), new Function<Node, Coordinate>() {
 			@Override
 			public Coordinate apply(final Node node) {
