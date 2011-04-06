2b49fb1c9e93ebb66fa89576ee9eef6e63b86d3f
John I. Gakos <gakos.ioannis@gmail.com>
2011-04-06 15:14:42 +0300

    Fixing previous commit by removing unused files and resources.

--- client/src/main/java/eu/wisebed/wiseui/client/reservation/view/resources/PublicReservationsResources.java
+++ client/src/main/java/eu/wisebed/wiseui/client/reservation/view/resources/PublicReservationsResources.java
@@ -2,15 +2,11 @@ package eu.wisebed.wiseui.client.reservation.view.resources;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.ImageResource;
 
 /**
  * Resources used by calendar widget.
  */
 public interface PublicReservationsResources extends ClientBundle {
-  
-	@Source("eu/wisebed/wiseui/client/reservation/static/underconstruction.png")
-	ImageResource underConstruction();
 
 	@Source("eu/wisebed/wiseui/client/reservation/css/publicReservations.css")
 	public Style style();
